03229376f3ca32301a8a2d8fa94991bc24faaad8
NicolasBQ <nicolasbquintero@gmail.com>
2022-12-01 20:09:55 -0500

    Handlebars

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 // Initialization
@@ -7,6 +8,13 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 4000); // Listen to the right PORT 
 app.set('views', path.join(__dirname, 'views')); // Found the views directory
+app.engine('.hbs', exphbs.engine({  
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs')
 
 // Middlewares
 app.use(express.urlencoded({extended: false})); //  Turn data into a JSON object
@@ -15,7 +23,7 @@ app.use(express.urlencoded({extended: false})); //  Turn data into a JSON object
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
 })
 
 // Static Files
